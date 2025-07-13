87ba379c5d190a57e21fe21af6a573bfce069736
dontwait <nguyentansang15042005@gmail.com>
2025-07-13 15:37:24 +0700

    fix: changing name id

--- shopapp/src/main/java/com/dontwait/shopapp/repository/OrderDetailRepository.java
+++ shopapp/src/main/java/com/dontwait/shopapp/repository/OrderDetailRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-    List<OrderDetail> findByOrderId(Long orderId);
+    List<OrderDetail> findByOrderOrderId(Long orderId); //order.orderId
 
 }
